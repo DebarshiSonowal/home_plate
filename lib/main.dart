a0046882b4310e69183401340b23e03cfab4525c
Debarshi Sonowal <debarkhisonowal@gmail.com>
2024-06-14 22:35:24 +0530

    Stable Before Background Service:

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,34 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:home_plate/Constants/constants.dart';
 import 'package:home_plate/Storage/CustomStorage.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'Navigation/Navigate.dart';
 import 'Repository/repository.dart';
 import 'Repository/theme.dart';
 import 'Router/router.dart';
 
-void main() async{
+const notificationChannelId = 'my_foreground';
+
+// this will be used for notification id, So you can update your custom notification with this id.
+const notificationId = 888;
+
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await initializeService();
   await dotenv.load(fileName: ".env");
   await Storage.instance.initializeStorage();
-  Future.delayed(const Duration(seconds: 4),()=>FlutterNativeSplash.remove());
+  Future.delayed(
+      const Duration(seconds: 4), () => FlutterNativeSplash.remove());
   runApp(const MyApp());
 }
 
@@ -62,7 +74,71 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
 
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    notificationChannelId, // id
+    'MY FOREGROUND SERVICE', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.low, // importance must be at low or higher level
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+
+      notificationChannelId: notificationChannelId,
+      // this must match with notification channel you created above.
+      initialNotificationTitle: 'AWESOME SERVICE',
+      initialNotificationContent: 'Initializing',
+      foregroundServiceNotificationId: notificationId,
+    ),
+    iosConfiguration: IosConfiguration(),
+  );
+}
+
+Future<void> onStart(ServiceInstance service) async {
+  // Only available for flutter 3.0.0 and later
+  DartPluginRegistrant.ensureInitialized();
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  // bring to foreground
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    if (service is AndroidServiceInstance) {
+      if (await service.isForegroundService()) {
+        flutterLocalNotificationsPlugin.show(
+          notificationId,
+          'COOL SERVICE',
+          'Awesome ${DateTime.now()}',
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              notificationChannelId,
+              'MY FOREGROUND SERVICE',
+              icon: 'ic_bg_service_small',
+              ongoing: true,
+            ),
+          ),
+        );
+      }
+    }
+  });
+}
 
 String convertPhoneNumber(String phoneNumber) {
   return phoneNumber.replaceFirst(RegExp(r'^\+1\s*'), '');
